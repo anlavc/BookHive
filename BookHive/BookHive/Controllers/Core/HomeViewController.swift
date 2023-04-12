e945313aaeb8aadd05910c6d8e4425f3d9ee14f2
Anıl Avcı <anilavci52@gmail.com>
2023-04-12 18:27:43 +0300

    bug fix

--- BookHive/BookHive/Controllers/Core/HomeViewController.swift
+++ BookHive/BookHive/Controllers/Core/HomeViewController.swift
@@ -12,9 +12,7 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-
 
     /*
     // MARK: - Navigation
