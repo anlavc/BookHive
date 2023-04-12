459174dc8a5abab072b8322d45fc9b18cb2b509a
Mehdican Buyukplevne <mbuyukplevne@hotmail.com>
2023-04-12 18:09:43 +0300

    Feature Core Controllers

---
+++ BookHive/BookHive/Controllers/Core/AccountViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AccountViewController.swift
+//  BookHive
+//
+//  Created by Mehdican Büyükplevne on 12.04.2023.
+//
+
+import UIKit
+
+class AccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BookHive/BookHive/Controllers/Core/MyBooksViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MyBooksViewController.swift
+//  BookHive
+//
+//  Created by Mehdican Büyükplevne on 12.04.2023.
+//
+
+import UIKit
+
+class MyBooksViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
