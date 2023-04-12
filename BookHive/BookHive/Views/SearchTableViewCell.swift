f0e82f22ede4e7bfd0fe90403c834dff3ac750d5
Mehdican Buyukplevne <mbuyukplevne@hotmail.com>
2023-04-12 20:07:19 +0300

    Feature SearchTableView and SearchBar

---
+++ BookHive/BookHive/Views/SearchTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  SearchTableViewCell.swift
+//  BookHive
+//
+//  Created by Mehdican Büyükplevne on 12.04.2023.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var bookImageView: UIImageView!
+    @IBOutlet weak var bookNameLabel: UILabel!
+    @IBOutlet weak var genreLabel: UILabel!
+    
+    
+    
+    static let identifier = "SearchTableViewCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    static func nib() -> UINib {
+        return UINib(nibName: "SearchTableViewCell", bundle: nil)
+    }
+    
+}
