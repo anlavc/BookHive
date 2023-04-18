84e0148136678da7b26cba52aa80b53c0c93a156
Mehdican Buyukplevne <mbuyukplevne@hotmail.com>
2023-04-18 09:40:36 +0300

    🤯[DEV] Create Favorite Button

--- BookHive/BookHive/Views/SearchTableViewCell/SearchTableViewCell.swift
+++ BookHive/BookHive/Views/SearchTableViewCell/SearchTableViewCell.swift
@@ -9,8 +9,13 @@ import UIKit
 
 class SearchTableViewCell: UITableViewCell {
     
+    var isFavorited = false
+        
     @IBOutlet weak var searchView: UIView!
     @IBOutlet weak var bookImageView: UIImageView!
+    @IBOutlet weak var favoriteView: UIView!
+    @IBOutlet weak var favoriteButton: UIButton!
+    
     
     static let identifier = "SearchTableViewCell"
 
@@ -26,5 +31,14 @@ class SearchTableViewCell: UITableViewCell {
     private func viewConfigure() {
         searchView.layer.cornerRadius = 20
         searchView.addShadow(color: .gray, opacity: 0.5, offset: CGSize(width: 2, height: 2), radius: 5)
+        favoriteView.addShadow(color: .gray, opacity: 0.5, offset: CGSize(width: 2, height: 2), radius: 5)
+        favoriteView.layer.cornerRadius = 10
+    }
+    
+    @IBAction func favoriteButtonTapped(_ sender: UIButton) {
+        isFavorited.toggle()
+        let bookFavorite = isFavorited ? "heart.fill" : "heart"
+        favoriteButton.setImage(UIImage(systemName: bookFavorite), for: .normal)
+        
     }
 }
