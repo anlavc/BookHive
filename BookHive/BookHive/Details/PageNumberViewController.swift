8518813b733de622df194fec838114aa46fe9c19
Mehdican Buyukplevne <mbuyukplevne@hotmail.com>
2023-04-27 23:29:48 +0300

    fixed

---
+++ BookHive/BookHive/Details/PageNumberViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PageNumberViewController.swift
+//  BookHive
+//
+//  Created by Mehdican Büyükplevne on 27.04.2023.
+//
+
+import UIKit
+
+class PageNumberViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
