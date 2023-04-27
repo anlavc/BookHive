4ea0fe515de209212e92c0fb2252d8c748d4204e
Mehdican Buyukplevne <mbuyukplevne@hotmail.com>
2023-04-27 12:01:24 +0300

    🤯[DEV] Create Desing + Create Localized Files About Pages

---
+++ BookHive/BookHive/Controllers/Details/AboutViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AboutViewController.swift
+//  BookHive
+//
+//  Created by Mehdican Büyükplevne on 26.04.2023.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    
+    @IBOutlet weak var aboutLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+
+    }
+    @IBAction func backButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
