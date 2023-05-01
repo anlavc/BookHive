49722c2f955b184cdd2d43e6ad844b0d9d3fb1d6
Mehdican Buyukplevne <mbuyukplevne@hotmail.com>
2023-05-01 21:52:10 +0300

    🤯[DEV] Create ReadListCollectionViewCell

--- BookHive/BookHive/Details/ReadListViewController.swift
+++ BookHive/BookHive/Details/ReadListViewController.swift
@@ -22,6 +22,15 @@ class ReadListViewController: UIViewController {
         fetchFavoriteBooks()
     }
     
+    private func setCollectionView() {
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(ReadListCollectionViewCell.nib(), forCellWithReuseIdentifier: ReadListCollectionViewCell.identifier)
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        collectionView.collectionViewLayout = layout
+    }
+    
     private func fetchFavoriteBooks() {
         if let uuid = Auth.auth().currentUser?.uid { // kullanıcının id sine ulaştım
             let favoriteBooksCollection = Firestore.firestore().collection("users/\(uuid)/favoriteBooks") //kullanıcıya özel oluşturulmuş collectiona ulaştım
@@ -47,13 +56,6 @@ class ReadListViewController: UIViewController {
             }
         }
     }
-    
-    
-    private func setCollectionView() {
-        collectionView.dataSource = self
-        collectionView.delegate = self
-        collectionView.register(ReadListCollectionViewCell.nib(), forCellWithReuseIdentifier: ReadListCollectionViewCell.identifier)
-    }
 
     @IBAction func backButton(_ sender: Any) {
         dismiss(animated: true)
@@ -62,19 +64,33 @@ class ReadListViewController: UIViewController {
 
 extension ReadListViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return favoriteBooks.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ReadListCollectionViewCell.identifier, for: indexPath) as! ReadListCollectionViewCell
+        cell.configure(with: favoriteBooks[indexPath.row])
         return cell
     }
 }
 
 extension ReadListViewController: UICollectionViewDelegate {
-    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let vc = PageNumberViewController()
+        vc.modalPresentationStyle = .fullScreen
+        present(vc, animated: true)
+    }
 }
 
 extension ReadListViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let cellWidth = (collectionView.bounds.width) / 2.2
+        let cellHeight = cellWidth * 1.7
+        return CGSize(width: cellWidth, height: cellHeight)
+    }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+    }
 }
+
